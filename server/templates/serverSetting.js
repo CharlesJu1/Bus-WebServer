50abb22fb5531c8369dbb93571cb19741c7d1297
CharlesJu1 <charlesyju@gmail.com>
2016-06-08 23:19:13 +0800

    create busmongodb in West US region from mlab.com web site.

--- server/templates/serverSetting.js
+++ server/templates/serverSetting.js
@@ -11,7 +11,12 @@ cfg.mongoDbBusUrl = 'mongodb://localhost:27017/bus';
 // @endif
 // @if SERVER_ENV == 'PRODUCTION'
 console.log('Node server running in production environment.');
-cfg.mongoDbBusUrl = 'mongodb://nodejsclient:nodejsclient@ds052968.mlab.com:52968/WMillMongoDB';
+// busmongodb is in west US region. It is created from mlab.com. 
+// manage.windowsazure.com does not have west US as region to create mongodb.
+cfg.mongoDbBusUrl = 'mongodb://nodejsclient:nodejsclient@ds034279.mlab.com:34279/busmongodb';
+
+//WMillMongoDB is created from Azure. Somehow it is created in North Europe region.
+//cfg.mongoDbBusUrl = 'mongodb://nodejsclient:nodejsclient@ds052968.mlab.com:52968/WMillMongoDB';
 // @endif
 cfg.busRouteCollection = 'route';
 cfg.busPositionCollection = 'position';
--- serverSetting.js
+++ serverSetting.js
@@ -3,7 +3,12 @@
 var cfg = {};
 
 console.log('Node server running in production environment.');
-cfg.mongoDbBusUrl = 'mongodb://nodejsclient:nodejsclient@ds052968.mlab.com:52968/WMillMongoDB';
+// busmongodb is in west US region. It is created from mlab.com. 
+// manage.windowsazure.com does not have west US as region to create mongodb.
+cfg.mongoDbBusUrl = 'mongodb://nodejsclient:nodejsclient@ds034279.mlab.com:34279/busmongodb';
+
+//WMillMongoDB is created from Azure. Somehow it is created in North Europe region.
+//cfg.mongoDbBusUrl = 'mongodb://nodejsclient:nodejsclient@ds052968.mlab.com:52968/WMillMongoDB';
 cfg.busRouteCollection = 'route';
 cfg.busPositionCollection = 'position';
 
